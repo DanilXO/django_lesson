07e113db330c1f1b6339ef5bb063582142b3b083
Frolov <byka72@gmail.com>
2019-03-18 18:18:35 +0400

    Initial

---
+++ polls/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from django.contrib.auth.models import User
+
+
+class LogInForm(forms.Form):
+    username = forms.CharField(label='Login: ', max_length=100)
+    password = forms.CharField(label='Password: ',widget=forms.PasswordInput())
+
+
+class SignUpForm(forms.Form):
+    your_name = forms.CharField(label='Your name', max_length=100)
