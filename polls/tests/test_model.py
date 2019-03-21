575424b211a1044d8d2cb82170b3c6441cfa3034
Frolov <byka72@gmail.com>
2019-03-21 17:25:03 +0400

    lesson 5

--- polls/tests/test_model.py
+++ polls/tests/test_model.py
@@ -9,6 +9,15 @@ from polls.models import Question
 
 class QuestionMethodTests(TestCase):
 
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_published_recently() should return False for questions whose
+        pub_date is in the future.
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertEqual(future_question.was_published_recently(), False)
+        
     def test_was_published_recently_with_old_question(self):
         """
         was_published_recently () должен возвращать False для вопросов, чьи
@@ -16,7 +25,8 @@ class QuestionMethodTests(TestCase):
         """
         time = timezone.now() - datetime.timedelta(days=30)
         old_question = Question(pub_date=time)
-        self.assertEqual(old_question.was_published_recently(), False)
+        self.assertFalse(old_question.was_published_recently())
+        # self.assertEqual(old_question.was_published_recently(), False)
 
     def test_was_published_recently_with_recent_question(self):
         """
@@ -25,4 +35,5 @@ class QuestionMethodTests(TestCase):
         """
         time = timezone.now() - datetime.timedelta(hours=1)
         recent_question = Question(pub_date=time)
-        self.assertEqual(recent_question.was_published_recently(), True)
+        self.assertTrue(recent_question.was_published_recently())
+        # self.assertEqual(recent_question.was_published_recently(), True)
